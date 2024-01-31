b40b29045982cdcb4e039b615ce8f7d81fa8dba2
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-01-31 15:02:05 +0700

    Add 104 for plin

---
+++ 104_plink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -N 1 -c 96
+#SBATCH -t 24:00:00
+#SBATCH --mem=100GB
+#SBATCH -J plink
+#SBATCH -A proj5034
+
+source ~/.bashrc
+
+mkdir -p data/plink
+plink --allow-extra-chr --make-bed --double-id --threads 96 \
+  --vcf data/populations/populations.snps.vcf \
+  --out data/plink
+echo "Plink done"
