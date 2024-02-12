4a98492ce02d1973b50875b0bcfa39b5f857b467
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-02-12 13:38:52 +0700

    Add 505 and 506

--- 505_plink.sh
+++ 505_plink.sh
@@ -8,9 +8,11 @@
 
 export PATH=$PATH:/tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/Softwares
 
+# TODO: Update the sample_list.txt file
 mkdir -p data/onref_plink
 /tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/Softwares/plink \
   --allow-extra-chr --make-bed --double-id --threads 96 \
   --vcf data/onref_populations/populations.snps.vcf \
+  --keep data/sample_list.txt \
   --out data/onref_plink/plink
 echo "Plink done"
