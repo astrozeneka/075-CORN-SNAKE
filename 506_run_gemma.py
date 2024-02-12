143fb69957879c57e8999ee95fe4fb42ac35b37d
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-02-12 13:39:37 +0700

    Add 506

--- 506_run_gemma.py
+++ 506_run_gemma.py
@@ -1,4 +1,5 @@
 import itertools
+import os
 
 phenotypes = [
     "AMEL",
@@ -35,4 +36,35 @@ if __name__ == '__main__':
         nc['normal'] = list(nc['normal'])
         nc['case'] = list(nc['case'])
         print(f"Normal: {nc['normal']}, Case: {nc['case']}")
-        print()
+        sample_list_content = """AMEL_106.sorted
+AMEL_13.sorted
+AMEL_14.sorted
+AMEL_15.sorted
+AMEL_20.sorted
+AMEL_23.sorted
+AMEL_28.sorted
+AMEL_29.sorted
+AMEL_30.sorted
+AMEL_44.sorted
+AMEL_48.sorted
+AMEL_51.sorted
+AMEL_71.sorted
+AMEL_77.sorted
+AMEL_84.sorted
+AMEL_96.sorted
+AMEL_97.sorted
+AMEL_99.sorted
+NOR_10.sorted
+NOR_11.sorted
+NOR_12.sorted
+NOR_4.sorted
+NOR_5.sorted
+NOR_69.sorted
+NOR_73.sorted
+NOR_7.sorted"""
+        open("data/sample_list.txt", "w").write(sample_list_content)
+        # Run bash command
+        print(f"bash 505_plink.sh")
+        os.system(f"bash 505_plink.sh")
+        print("Done")
+        break
